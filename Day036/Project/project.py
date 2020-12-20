87cd3797ee63781e2a432d411ac7e044091fc928
pullynnhah <pullynnhah@protonmail.com>
2020-12-20 11:46:27 -0300

    Day036

--- Day036/Project/project.py
+++ Day036/Project/project.py
@@ -1,9 +1,26 @@
 import os
-import json
 import html
 import requests
 from newsapi import NewsApiClient
 from twilio.rest import Client
+from datetime import datetime, timedelta
+
+today = datetime.now()
+
+
+def get_previous_date(date):
+    if date.weekday() == 0:
+        return date - timedelta(days=3)
+    elif date.weekday() == 6:
+        return date - timedelta(days=2)
+    return date - timedelta(days=1)
+
+
+day = get_previous_date(today)
+last_date = f'{day.year}-{day.month}-{day.day}'
+day = get_previous_date(day)
+before_last_date = f'{day.year}-{day.month}-{day.day}'
+today = f'{today.year}-{today.month}-{today.day}'
 
 account_sid = os.environ["TWILIO_SID"]
 auth_token = os.environ["TWILIO_TOKEN"]
@@ -15,8 +32,6 @@ my_number = os.environ["MY_NUMBER"]
 STOCK = "TSLA"
 COMPANY_NAME = "Tesla Inc"
 ALPHAVANTAGE_URL = "https://www.alphavantage.co/query"
-YESTERDAY = "2020-12-17"
-TODAY = "2020-12-18"
 NEWS_URL = "https://newsapi.org/v2/everything"
 
 stock_params = {
@@ -30,13 +45,13 @@ response = requests.get(ALPHAVANTAGE_URL, params=stock_params)
 response.raise_for_status()
 stock_data = response.json()
 
-close_yesterday = float(stock_data["Time Series (Daily)"][YESTERDAY]["4. close"])
-close_today = float(stock_data["Time Series (Daily)"][TODAY]["4. close"])
+close_yesterday = float(stock_data["Time Series (Daily)"][last_date]["4. close"])
+close_today = float(stock_data["Time Series (Daily)"][before_last_date]["4. close"])
 abs_percentage = abs((close_today - close_yesterday) * 100 / close_yesterday)
 if abs_percentage >= 5:
     news_api = NewsApiClient(api_key=os.environ["NEWS_API_KEY"])
-    all_articles = news_api.get_everything(q=COMPANY_NAME, from_param=YESTERDAY,
-                                           to=TODAY, language='en')
+    all_articles = news_api.get_everything(q=COMPANY_NAME, from_param=last_date,
+                                           to=today, language='en')
 
     articles = all_articles["articles"][:3]
     news = ""
--- Day036/Project/project01.py
+++ Day036/Project/project01.py
@@ -1,12 +1,27 @@
 import os
 import requests
-import newsapi
+from datetime import datetime, timedelta
+
+today = datetime.now()
+
+
+def get_previous_date(date):
+    if date.weekday() == 0:
+        return date - timedelta(days=3)
+    elif date.weekday() == 6:
+        return date - timedelta(days=2)
+    return date - timedelta(days=1)
+
+
+day = get_previous_date(today)
+last_date = f'{day.year}-{day.month}-{day.day}'
+day = get_previous_date(day)
+before_last_date = f'{day.year}-{day.month}-{day.day}'
+today = f'{today.year}-{today.month}-{today.day}'
 
 STOCK = "TSLA"
 COMPANY_NAME = "Tesla Inc"
 ALPHAVANTAGE_URL = "https://www.alphavantage.co/query"
-YESTERDAY = "2020-12-17"
-TODAY = "2020-12-18"
 
 stock_params = {
     "function": "TIME_SERIES_DAILY",
@@ -19,8 +34,8 @@ response = requests.get(ALPHAVANTAGE_URL, params=stock_params)
 response.raise_for_status()
 stock_data = response.json()
 
-close_yesterday = float(stock_data["Time Series (Daily)"][YESTERDAY]["4. close"])
-close_today = float(stock_data["Time Series (Daily)"][TODAY]["4. close"])
+close_yesterday = float(stock_data["Time Series (Daily)"][last_date]["4. close"])
+close_today = float(stock_data["Time Series (Daily)"][before_last_date]["4. close"])
 abs_percentage = abs((close_today - close_yesterday) * 100 / close_yesterday)
 if abs_percentage >= 5:
     print("Get News")
